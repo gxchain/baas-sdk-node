76acafc48fc686a644cf4d963d75cb832fec9119
lanhaoxiang <lanhaoxiang@qq.com>
2018-07-05 20:13:06 +0800

    update examples

--- test/data.js
+++ test/data.js
@@ -1,8 +1,8 @@
 import BaaSClient from "../lib/client/BaaSClient";
 
 let client = new BaaSClient("5JnDdu5s4jFeQ7Kqovgdcae5t1spodJFPuJzs4Xpd88Grhx8GGV", "1.2.61", "https://baas-developer.gxchain.cn/api");
-client.getProviderInfo().then(info => {
-    console.log(JSON.stringify(info, null, "  "));
+client.data("QmaZrwThXyZm8Rxs93Tih3L6p4Q8NqYEXp32iN4PeAqDgv").then(resp => {
+    console.log(resp);
 }).catch(ex => {
-    console.log("error when get provider info", ex);
+    console.error("error when invoke data method:", ex);
 });
--- test/store.js
+++ test/store.js
@@ -1,8 +1,8 @@
 import BaaSClient from "../lib/client/BaaSClient";
 
 let client = new BaaSClient("5JnDdu5s4jFeQ7Kqovgdcae5t1spodJFPuJzs4Xpd88Grhx8GGV", "1.2.61", "https://baas-developer.gxchain.cn/api");
-client.getProviderInfo().then(info => {
-    console.log(JSON.stringify(info, null, "  "));
+client.store("test").then(resp => {
+    console.log(resp);
 }).catch(ex => {
-    console.log("error when get provider info", ex);
+    console.error("error when invoke store method:", ex);
 });
